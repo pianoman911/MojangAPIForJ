ddf63bf72a69f024c22fea7845302652347750e8
pianoman911 <pianofinn13@gmail.com>
2021-03-11 15:46:26 +0100

    Mojang KeyFile bypass

--- src/main/java/org/shanerx/mojang/Mojang.java
+++ src/main/java/org/shanerx/mojang/Mojang.java
@@ -16,43 +16,39 @@
 
 package org.shanerx.mojang;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.commons.codec.binary.Base64;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * <p>This class represents the connection with the Mojang API.
  * <p>All instances of other classes of this wrapper API should be retrieved through this class.
  * <p>Remember to call <code>api.connect()</code> after creating an instance of this class.
  */
-@SuppressWarnings({ "unchecked" })
-public class Mojang
-{
+@SuppressWarnings({"unchecked"})
+public class Mojang {
 
     private Map<String, ServiceStatus> apiStatus;
 
     /**
      * Constructor. Initializes member variables.
      */
-    public Mojang()
-    {
+    public Mojang() {
         apiStatus = new HashMap<>();
     }
 
@@ -65,8 +61,7 @@ public class Mojang
      *
      * @return the api itself. Useful for concatenation.
      */
-    public Mojang connect()
-    {
+    public Mojang connect() {
         JSONArray arr = getJSONArray("https://status.mojang.com/check");
 
         for (int i = 0; i <= 7; ++i) {
@@ -86,13 +81,10 @@ public class Mojang
      * Check the enum entries for {@link org.shanerx.mojang.Mojang.ServiceStatus} for possible response types.
      *
      * @param service the service type
-     *
      * @return the status of said service
      */
-    public ServiceStatus getStatus(ServiceType service)
-    {
-        if (service == null)
-        {
+    public ServiceStatus getStatus(ServiceType service) {
+        if (service == null) {
             return ServiceStatus.UNKNOWN;
         }
         return apiStatus.get(service.toString());
@@ -102,11 +94,9 @@ public class Mojang
      * Retrieves the current UUID linked to a username.
      *
      * @param username the username
-     *
      * @return the UUID as a {@link java.lang.String String}
      */
-    public String getUUIDOfUsername(String username)
-    {
+    public String getUUIDOfUsername(String username) {
         return (String) getJSONObject("https://api.mojang.com/users/profiles/minecraft/" + username).get("id");
     }
 
@@ -115,11 +105,9 @@ public class Mojang
      *
      * @param username  the username
      * @param timestamp the Java Timestamp that represents the time
-     *
      * @return the UUID as a {@link java.lang.String String}
      */
-    public String getUUIDOfUsername(String username, String timestamp)
-    {
+    public String getUUIDOfUsername(String username, String timestamp) {
         return (String) getJSONObject("https://api.mojang.com/users/profiles/minecraft/" + username + "?at=" + timestamp).get("id");
     }
 
@@ -127,12 +115,10 @@ public class Mojang
      * Retrieves all the username a certain UUID has had in the past, including the current one.
      *
      * @param uuid the UUID
-     *
      * @return a map with the username as key value and the Timestamp as a {@link java.lang.Long Long}
      */
-    public Map<String, Long> getNameHistoryOfPlayer(String uuid)
-    {
-        JSONArray         arr     = getJSONArray("https://api.mojang.com/user/profiles/" + uuid + "/names");
+    public Map<String, Long> getNameHistoryOfPlayer(String uuid) {
+        JSONArray arr = getJSONArray("https://api.mojang.com/user/profiles/" + uuid + "/names");
         Map<String, Long> history = new HashMap<>();
         arr.forEach(o ->
         {
@@ -146,47 +132,46 @@ public class Mojang
      * Returns the {@link org.shanerx.mojang.PlayerProfile PlayerProfile} object which holds and represents the metadata for a certain account.
      *
      * @param uuid the UUID of the player
-     *
-	 * @return the {@link org.shanerx.mojang.PlayerProfile PlayerProfile} object}
-	 */
-	public PlayerProfile getPlayerProfile(String uuid) {
-		JSONObject obj = getJSONObject("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
-		String name = (String) obj.get("name");
-		Set<PlayerProfile.Property> properties = (Set<PlayerProfile.Property>) ((JSONArray) obj.get("properties")).stream().map(o -> {
-			PlayerProfile.Property p;
-			JSONObject prop = (JSONObject) o;
+     * @return the {@link org.shanerx.mojang.PlayerProfile PlayerProfile} object}
+     */
+    public PlayerProfile getPlayerProfile(String uuid) {
+        JSONObject obj = getJSONObject("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
+        String name = (String) obj.get("name");
+        Set<PlayerProfile.Property> properties = (Set<PlayerProfile.Property>) ((JSONArray) obj.get("properties")).stream().map(o -> {
+            PlayerProfile.Property p;
+            JSONObject prop = (JSONObject) o;
 
-			String propName = (String) prop.get("name");
-			String propValue = (String) prop.get("value");
-			if (propName.equals("textures")) {
-				JSONObject tex;
-				try {
-					tex = (JSONObject) new JSONParser().parse(new String(Base64.decodeBase64(propValue)));
-				} catch (ParseException e2) {
-					/* Don't blame me, I just follow the pattern from #getJSONObject */
-					throw new RuntimeException(e2);
-				}
-				PlayerProfile.TexturesProperty q = new PlayerProfile.TexturesProperty();
-				q.timestamp = (Long) tex.get("timestamp");
-				q.profileId = (String) tex.get("profileId");
-				q.profileName = (String) tex.get("profileName");
-				q.signatureRequired = Boolean.parseBoolean((String) tex.get("signatureRequired"));
-				q.textures = ((Stream<Entry<Object, Object>>) ((JSONObject) tex.get("textures")).entrySet().stream()).collect(Collectors.toMap(
-						e -> (String) e.getKey(),
-						e -> {
-							try {
-								return new URL((String) ((JSONObject) e.getValue()).get("url"));
-							} catch (MalformedURLException e1) {
-								/* I want lambdas with exceptions in Java, *please* */
-								throw new RuntimeException("Wrapper for checked exception for lambda", e1);
-							}
-						}));
-				p = q;
-			} else
-				p = new PlayerProfile.Property();
-			p.name = propName;
-			p.signature = (String) prop.get("signature");
-			p.value = propValue;
+            String propName = (String) prop.get("name");
+            String propValue = (String) prop.get("value");
+            if (propName.equals("textures")) {
+                JSONObject tex;
+                try {
+                    tex = (JSONObject) new JSONParser().parse(new String(Base64.decodeBase64(propValue)));
+                } catch (ParseException e2) {
+                    /* Don't blame me, I just follow the pattern from #getJSONObject */
+                    throw new RuntimeException(e2);
+                }
+                PlayerProfile.TexturesProperty q = new PlayerProfile.TexturesProperty();
+                q.timestamp = (Long) tex.get("timestamp");
+                q.profileId = (String) tex.get("profileId");
+                q.profileName = (String) tex.get("profileName");
+                q.signatureRequired = Boolean.parseBoolean((String) tex.get("signatureRequired"));
+                q.textures = ((Stream<Entry<Object, Object>>) ((JSONObject) tex.get("textures")).entrySet().stream()).collect(Collectors.toMap(
+                        e -> (String) e.getKey(),
+                        e -> {
+                            try {
+                                return new URL((String) ((JSONObject) e.getValue()).get("url"));
+                            } catch (MalformedURLException e1) {
+                                /* I want lambdas with exceptions in Java, *please* */
+                                throw new RuntimeException("Wrapper for checked exception for lambda", e1);
+                            }
+                        }));
+                p = q;
+            } else
+                p = new PlayerProfile.Property();
+            p.name = propName;
+            p.signature = (String) prop.get("signature");
+            p.value = propValue;
             return p;
         }).collect(Collectors.toSet());
         return new PlayerProfile(uuid, name, properties);
@@ -201,14 +186,10 @@ public class Mojang
      * @param skinType the {@link org.shanerx.mojang.Mojang.SkinType type} of the skin
      * @param skinUrl  a direct URL to the skin
      */
-    public void updateSkin(String uuid, String token, SkinType skinType, String skinUrl)
-    {
-        try
-        {
+    public void updateSkin(String uuid, String token, SkinType skinType, String skinUrl) {
+        try {
             Unirest.post("https://api.mojang.com/user/profile/" + uuid + "/skin").header("Authorization", "Bearer " + token).field("model", skinType.toString()).field("url", skinUrl).asString();
-        }
-        catch (UnirestException e)
-        {
+        } catch (UnirestException e) {
             e.printStackTrace();
         }
     }
@@ -223,14 +204,10 @@ public class Mojang
      * @param file     the raw image data
      */
     @Untested
-    public void updateAndUpload(String uuid, String token, SkinType skinType, String file)
-    {
-        try
-        {
+    public void updateAndUpload(String uuid, String token, SkinType skinType, String file) {
+        try {
             Unirest.put("https://api.mojang.com/user/profile/" + uuid + "/skin").header("Authorization", "Bearer " + token).field("model", skinType.toString().equals("") ? "alex" : skinType.toString()).field("file", file).asString();
-        }
-        catch (UnirestException e)
-        {
+        } catch (UnirestException e) {
             e.printStackTrace();
         }
     }
@@ -241,14 +218,10 @@ public class Mojang
      * @param uuid  the UUID of the player
      * @param token the token used for API authentication
      */
-    public void resetSkin(String uuid, String token)
-    {
-        try
-        {
+    public void resetSkin(String uuid, String token) {
+        try {
             Unirest.delete("https://api.mojang.com/user/profile/" + uuid + "/skin").header("Authorization", "Bearer " + token).asString();
-        }
-        catch (UnirestException e)
-        {
+        } catch (UnirestException e) {
             e.printStackTrace();
         }
     }
@@ -259,14 +232,10 @@ public class Mojang
      *
      * @return a {@link java.util.List List} of all the blocked hostnames
      */
-    public List<String> getServerBlacklist()
-    {
-        try
-        {
+    public List<String> getServerBlacklist() {
+        try {
             return Arrays.asList(Unirest.get("https://sessionserver.mojang.com/blockedservers").asString().getBody().split("\n"));
-        }
-        catch (UnirestException e)
-        {
+        } catch (UnirestException e) {
             e.printStackTrace();
             return null;
         }
@@ -276,23 +245,18 @@ public class Mojang
      * Returns the official mojang's product sales statistics.
      *
      * @param options the query {@link org.shanerx.mojang.SalesStats.Options options}
-     *
      * @return the stats
      */
     @Untested
-    public SalesStats getSaleStatistics(SalesStats.Options... options)
-    {
+    public SalesStats getSaleStatistics(SalesStats.Options... options) {
         JSONArray arr = new JSONArray();
         Collections.addAll(arr, options);
 
         SalesStats stats = null;
-        try
-        {
+        try {
             JSONObject resp = (JSONObject) new JSONParser().parse(Unirest.post("https://api.mojang.com/orders/statistics").field("metricKeys", arr).asString().getBody());
             stats = new SalesStats(Integer.valueOf((String) resp.get("total")), Integer.valueOf((String) resp.get("last24h")), Integer.valueOf((String) resp.get("saleVelocityPerSeconds")));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return stats;
@@ -301,8 +265,7 @@ public class Mojang
     /**
      * This enum represents the possible Mojang API servers availability statuses.
      */
-    public enum ServiceStatus
-    {
+    public enum ServiceStatus {
 
         RED,
         YELLOW,
@@ -313,8 +276,7 @@ public class Mojang
     /**
      * This enum represents the various portions of the Mojang API.
      */
-    public enum ServiceType
-    {
+    public enum ServiceType {
 
         MINECRAFT_NET,
         SESSION_MINECRAFT_NET,
@@ -333,8 +295,7 @@ public class Mojang
          * @return the string
          */
         @Override
-        public String toString()
-        {
+        public String toString() {
             return name().toLowerCase().replace("_", ".");
         }
     }
@@ -342,8 +303,7 @@ public class Mojang
     /**
      * This enum represents the skin types "Alex" and "Steve".
      */
-    public enum SkinType
-    {
+    public enum SkinType {
         /**
          * Steve
          */
@@ -359,51 +319,73 @@ public class Mojang
          * @return the string
          */
         @Override
-        public String toString()
-        {
+        public String toString() {
             return this == DEFAULT ? "" : "slim";
         }
     }
 
-    private static JSONObject getJSONObject(String url)
-    {
-        JSONObject obj;
+    private static JSONObject getJSONObject(String url) {
+        JSONObject obj = null;
 
-        try
-        {
-            obj = (JSONObject) new JSONParser().parse(Unirest.get(url).asString().getBody());
-            String err = (String) (obj.get("error"));
-            if (err != null)
-            {
-                switch (err)
-                {
-                    case "IllegalArgumentException":
-                        throw new IllegalArgumentException((String) obj.get("errorMessage"));
-                    default:
-                        throw new RuntimeException(err);
-                }
+        try {
+            //URL url = new URL("https://" + header.getResource().substring(8));
+            //System.out.println("https://" + header.getResource().substring(8) + " // " + url.toString());
+            HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
+
+
+            int responseCode = con.getResponseCode();
+            BufferedReader apiReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            List<String> inLines = new ArrayList<>();
+            while ((inputLine = apiReader.readLine()) != null) {
+                System.out.println(inputLine);
+                inLines.add(inputLine);
             }
-        }
-        catch (ParseException | UnirestException e)
-        {
-            throw new RuntimeException(e);
+            apiReader.close();
+
+            StringBuilder responseBody = new StringBuilder();
+
+            for (String test : inLines) {
+                responseBody.append(test).append("\r\n");
+            }
+
+            obj = (JSONObject) new JSONParser().parse(responseBody.toString());
+
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
         }
 
         return obj;
     }
 
-    private static JSONArray getJSONArray(String url)
-    {
-        JSONArray arr;
+    private static JSONArray getJSONArray(String url) {
+        JSONArray arr = null;
 
-        try
-        {
-            arr = (JSONArray) new JSONParser().parse(Unirest.get(url).asString().getBody());
+        try {
+            HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
 
-        }
-        catch (ParseException | UnirestException e)
-        {
+
+            int responseCode = con.getResponseCode();
+            BufferedReader apiReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            List<String> inLines = new ArrayList<>();
+            while ((inputLine = apiReader.readLine()) != null) {
+                System.out.println(inputLine);
+                inLines.add(inputLine);
+            }
+            apiReader.close();
+
+            StringBuilder responseBody = new StringBuilder();
+
+            for (String test : inLines) {
+                responseBody.append(test).append("\r\n");
+            }
+            arr = (JSONArray) new JSONParser().parse(responseBody.toString());
+
+        } catch (ParseException | MalformedURLException e) {
             throw new RuntimeException(e);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
 
         return arr;
